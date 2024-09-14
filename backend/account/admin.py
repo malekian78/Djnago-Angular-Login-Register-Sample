3a0d34c6b2665b79f105b1ab59e7612761a9daaf
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-09-14 20:43:16 +0430

    ✔ add account app to backend

---
+++ backend/account/admin.py
@@ -0,0 +1,49 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+from .models import User
+
+class CustomUserAdmin(UserAdmin):
+    model = User
+    list_display = ("email", "is_superuser", "is_active", "is_verified")
+    list_filter = ("email", "is_superuser", "is_active", "is_verified")
+    search_fields = ("email",)
+    ordering = ("email",)
+    fieldsets = (
+        (
+            "Authentication",
+            {
+                "fields": ("email", "password"),
+            },
+        ),
+        (
+            "Permissions",
+            {
+                "fields": (
+                    "is_staff",
+                    "is_verified",
+                    "is_active",
+                    "is_superuser",
+                ),
+            },
+        ),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "is_superuser",
+                    "is_verified",
+                ),
+            },
+        ),
+    )
+
+
+admin.site.register(User, CustomUserAdmin)
