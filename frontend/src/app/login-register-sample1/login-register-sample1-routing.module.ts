87fbb1237652ecfb224734f975931ce8f472e572
Mohammad-hosein Malekian <mh.malekian78@gmail.com>
2024-09-14 11:55:53 +0430

    ✔ adding router for login and register

--- frontend/src/app/login-register-sample1/login-register-sample1-routing.module.ts
+++ frontend/src/app/login-register-sample1/login-register-sample1-routing.module.ts
@@ -1,9 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginRegisterSample1Component } from './login-register-sample1.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
 
 const routes: Routes = [
-  {path:'', component:LoginRegisterSample1Component}
+  {path:"", component:LoginRegisterSample1Component, children:[
+    {path:"login", component:LoginComponent},
+    {path:"register", component:RegisterComponent},
+    {path:"**", redirectTo:"login"},
+  ]},
+
 ];
 
 @NgModule({
